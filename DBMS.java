8b0435c17edf3d8fc93673aaa1e13aca20d3c202
Renu Alias <renualiasmeleth@gmail.com>
2025-09-22 20:10:01 +0530

    Add files via upload

---
+++ DBMS.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBMS {
+    public static final String URL="jdbc:mysql://localhost:3306/hospital_ms";
+    public static final String USER="root";
+    public static final String PASSWORD="cornetto";
+
+    public static Connection DBConnection(){
+        Connection conn=null;
+        try{
+            conn=DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Connection successful ");
+        }
+        catch(SQLException e){
+            System.out.println("Connection failed: "+e.getMessage());
+        }
+        return conn;
+    }
+    public static void main(String[] args){
+        DBConnection();
+    }
+}
