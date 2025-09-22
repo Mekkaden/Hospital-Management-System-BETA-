d13875f95f1975e46a238237fc194135e11a7278
RockeyyAbraham <rockeyy.abraham@gmail.com>
2025-09-22 21:59:09 +0530

    Database connectivity code

---
+++ Dbconnection.java
@@ -0,0 +1,26 @@
+// Dbconnection.java
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbconnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/hospital_ms"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "password"; 
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connection successfull!");
+            } catch (SQLException e) {
+                System.out.println(" Connection Error: " + e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+}
